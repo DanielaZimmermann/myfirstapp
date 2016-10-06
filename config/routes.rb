455b3f0cf7767b5596dc73d499b14addedc41f41
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-06 22:57:43 +0200

    3.8 add static pages index about contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
