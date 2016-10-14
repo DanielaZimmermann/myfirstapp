4d9e75f0e503aea81028d813ffd0d213cab235ed
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-14 21:05:26 +0200

    4.4 add order model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013194130) do
+ActiveRecord::Schema.define(version: 20161014181725) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float   "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
