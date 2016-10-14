a5c3f9dab45c34b5d5ce88cfe7500cd3156275ed
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-14 17:01:18 +0200

    4.3 add landing_page and empty orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+
 	#root 'static_pages#index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   get 'static_pages/contact'
 
