02b026dc877e59da1ad00bfa969da415cda66187
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-17 15:47:06 +0200

    4.5 add precompiled assets and correct carousel-indicators

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
-    @products = Product.limit(3)
+    @products = Product.limit(5)
   end
 
   def skyscanner
