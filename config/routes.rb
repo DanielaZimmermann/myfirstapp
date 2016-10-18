3b56967a156f40edcbce3c93caa27614c4d039a8
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-18 21:50:40 +0200

    4.6 fix stylings

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
