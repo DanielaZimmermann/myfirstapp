b552da181320530f78e6818ebecec2d8fb8db586
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-31 18:50:53 +0100

    5.10 add admin flag

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,11 +15,15 @@ class CommentsController < ApplicationController
 	end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
 
-private
+  private
 
-  def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
-  end
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
+    end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    can :destroy, Comment, user.admin: true
     #can :manage, Order, id: user.order.id
   end
 end
