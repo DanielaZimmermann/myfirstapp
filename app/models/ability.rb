76c17f3e36aa8fce940e4d89dbe4e64c0b2a48be
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-10-31 19:26:14 +0100

    5.10 fix only admin can delete comment

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-    can :destroy, Comment, user.admin: true
+    if user.admin?
+      can :destroy, Comment
+    end
     #can :manage, Order, id: user.order.id
   end
 end
