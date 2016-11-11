19c0ef3968883ea357b491e728fa1f5114ffb383
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-11-11 17:24:21 +0100

    6.3 controller spec

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem 'sqlite3'
   # rspec for bdd testing
   gem 'rspec-rails', '~> 3.5'
+  gem 'rails-controller-testing'
 
 end
 
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  before do
+    @user = User.create!(email: "test@test.de", password: "testtest")
+  end
+
+  describe 'GET #show' do
+
+     context 'User is logged in' do
+      before do
+        sign_in user
+      end
+      it 'loads correct user details' do
+        get :show, id: @user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+
+     end
+
+     context 'No user is logged in' do
+       it 'redirects to login' do
+         get :show, id: @user.id
+         expect(response).to redirect_to(root_path)
+       end
+     end
+  end
+
+
+end
