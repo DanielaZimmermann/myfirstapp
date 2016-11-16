28054fd98b3f34ceca953e991d0a92badf8bf289
DZimmermann <dani.zimmermann.reg@gmail.com>
2016-11-16 21:13:12 +0100

    6.9 - 6.10 elevateZoom and ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,20 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
+};
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+    /* raty plugin */
+    refreshRating();
+
+    /* elevate zoom plugin*/
+    $(".img-zoom").elevateZoom({
+
+		});
 });
